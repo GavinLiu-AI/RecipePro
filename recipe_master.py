435791046ae8724ea73defdee9640f1593c183db
chrisli <chrisli@nvidia.com>
2021-03-06 23:36:21 -0800

    added main files

---
+++ recipe_master.py
@@ -0,0 +1,167 @@
+import pickle
+import os
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+# Save recipe map as image to recipe
+def save_map(recipe_map):
+    with open('recipe-map/recipes.pkl', 'wb') as f:
+        print('Recipe map saved')
+        print(recipe_map)
+        pickle.dump(recipe_map, f)
+
+
+# Load recipe map
+def load_map():
+    with open('recipe-map/recipes.pkl', 'rb') as f:
+        data = pickle.load(f)
+        print('Recipe map loaded')
+        return data
+
+
+def save_last_recipe(recipe):
+    with open('recipe-map/last_recipe.pkl', 'wb') as f:
+        print('\nLast recipe saved')
+        print(recipe)
+        pickle.dump(recipe, f)
+
+
+def load_last_recipe():
+    with open('recipe-map/last_recipe.pkl', 'rb') as f:
+        data = pickle.load(f)
+        print('\nLast recipe loaded')
+        print(data)
+        return data
+
+
+# Get current images in the directory
+def get_dir_images(extension):
+    image_dir = 'images'
+
+    # Get all image names without extension
+    image_list = []
+    for filename in os.listdir(image_dir):
+        if not extension:
+            filename = os.path.splitext(filename)[0]
+        image_list.append(filename)
+    return image_list
+
+
+# Add new recipes
+def create_mode():
+    print('-------------------------------')
+    print('Creating new recipes\n')
+
+    image_list = get_dir_images(extension=True)
+
+    fig = plt.figure(figsize=(10, 10))
+    fig.set_tight_layout(True)
+    plt.axis('off')
+
+    recipe_map = load_map()
+    for image in image_list:
+        # image_name = os.path.splitext(image)[0]
+        if image not in recipe_map:
+            # Image name without extension
+            plt.ion()
+            recipe_img = mpimg.imread('images/' + image)
+            plt.imshow(recipe_img, aspect='auto')
+            plt.waitforbuttonpress()
+
+            # Enter one line
+            recipe_map[image] = str(input('\nRecipe for ' + image + ': ')).lower()
+            plt.clf()
+
+    save_map(recipe_map)
+    return
+
+
+# TODO: edit mode for recipe correction
+def edit_mode():
+    pass
+
+
+def editor(create, edit):
+    if create:
+        try:
+            create_mode()
+            print('Added new recipes successfully\n\n')
+        except:
+            print('ERROR: Create failed')
+    if edit:
+        try:
+            edit_mode()
+            print('Editted recipes successful\n\n')
+        except:
+            print('ERROR: Edit failed')
+
+
+# Update recipes based on current images
+def remove_recipes():
+    print('-------------------------------')
+    print('Removing old recipes\n')
+    recipe_map = load_map()
+    image_list = get_dir_images(extension=False)
+
+    remove_list = []
+    for key in list(recipe_map.keys()):
+        if key not in image_list:
+            remove_list.append(key)
+
+    # Skip if nothing to remove
+    if not remove_list:
+        print('SKIPPED: Nothing to remove')
+        return
+
+    # Confirm to remove recipes
+    print('About to remove: ', remove_list)
+    confirm = str(input('Confirm? (y/n): ')).lower()
+    if confirm == 'y':
+        for key in remove_list:
+            recipe_map.pop(key)
+        save_map(recipe_map)
+        print('UPDATED: removed old recipes\n\n')
+    else:
+        print('Aborted')
+    return
+
+
+def clear_recipe():
+    # Confirm to remove recipes
+    print('About to clear ALL recipes')
+    confirm = str(input('Confirm? (y/n): ')).lower()
+
+    if confirm == 'y':
+        recipe_map = dict()
+        save_map(recipe_map)
+    return
+
+
+def search_recipe(search):
+    recipe_map = load_map()
+
+    result = [key for key, value in recipe_map.items() if search in value]
+    recipe = [value for _, value in recipe_map.items() if search in value]
+
+    print('\nFound: ')
+    for r in recipe:
+        print(r)
+
+    return result
+
+
+def run(create, edit, remove, clear):
+    if clear:
+        clear_recipe()
+    if remove:
+        remove_recipes()
+    if create or edit:
+        editor(create, edit)
+    return
+
+
+if __name__ == '__main__':
+    run(create=True, edit=False, remove=False, clear=False)
+    # load_map()
+    # search_recipe('cookies')
---
+++ recipe_search.py
@@ -0,0 +1,129 @@
+import tweepy
+import time
+import logging
+from datetime import datetime
+import pickle
+
+API_KEY = 'Os4mmoHxVlTDexaoBI6pPltYb'
+API_SECRET = 'PCgtTiGWubdnp1fHyDpPyP3PdopULxsc3i34KMELABPkslYHy3'
+ACCESS_TOKEN = '1368266407125733376-BIH1qjjXngmnOZwmjQZzfVJLoCizMI'
+ACCESS_SECRET = '2OBxdizhvn5mY0WG3UwlFDWNbQt2G9PUhXKdkUznrRtIM'
+
+auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+user = api.me()
+
+
+# Load recipe map
+def load_map():
+    with open('recipe-map/recipes.pkl', 'rb') as f:
+        data = pickle.load(f)
+        print('Recipe map loaded')
+        return data
+
+
+def save_last_recipe(recipe):
+    with open('recipe-map/last_recipe.pkl', 'wb') as f:
+        print('\nLast recipe saved')
+        print(recipe)
+        pickle.dump(recipe, f)
+
+
+def load_last_recipe():
+    with open('recipe-map/last_recipe.pkl', 'rb') as f:
+        data = pickle.load(f)
+        print('\nLast recipe loaded')
+        print(data)
+        return data
+
+
+def search_recipe(search):
+    recipe_map = load_map()
+
+    result = [key for key, value in recipe_map.items() if search in value]
+    recipe = [value for _, value in recipe_map.items() if search in value]
+
+    print('\nFound: ')
+    for r in recipe:
+        print(r)
+
+    return result
+
+
+def get_time():
+    now = datetime.now()
+    return now.strftime("%H:%M:%S")
+
+
+def get_page(index, total_page):
+    return '(' + str(index + 1) + '/' + str(total_page) + ')'
+
+
+def send_recipe():
+    print('\nSearching for: ', direct_message)
+
+    result = search_recipe(direct_message)
+
+    if result:
+        # Break results into chunks of 4
+        result = [result[i:i + 4] for i in range(0, len(result), 4)]
+
+        total_page = len(result)
+        for index, recipe_group in enumerate(result):
+            # Upload images and get media_ids
+            media_ids = []
+            for recipe in recipe_group:
+                res = api.media_upload('images/' + str(recipe))
+                media_ids.append(res.media_id)
+
+            # Tweet with multiple images
+            status_text = 'Recipes for ' + direct_message + ' ' + get_page(index, total_page)
+            api.update_status(status=status_text, media_ids=media_ids)
+            if index == len(result) - 1:
+                print('\nAll recipes tweeted. DONE!')
+            else:
+                print('\nTweeted some recipes, more to go!')
+            time.sleep(5)
+
+        api.send_direct_message(sender_id,
+                                'I found the recipes for ' + direct_message + '. Check my tweets! ' + get_time())
+        print('\nUser notified via DM.')
+    else:
+        print('\nNo recipes found')
+        api.send_direct_message(sender_id, 'Sorry! I could not find recipes for ' + direct_message + '. ' + get_time())
+
+    save_last_recipe(direct_message)
+    return
+
+
+while True:
+    try:
+        print('-------------------------------')
+        # My own sender_id = '1368266407125733376'
+        dms = api.list_direct_messages()
+        while dms[0].message_create['sender_id'] == '1368266407125733376':
+            dms.pop(0)
+        direct_message = str(dms[0].message_create['message_data']['text']).lower()
+        print('DM Request: ', direct_message)
+        sender_id = dms[0].message_create['sender_id']
+
+        # Load last recipe
+        last_recipe = load_last_recipe()
+
+        direct_message = direct_message.split(' ')
+        if 'find' == direct_message[0]:
+            direct_message.pop(0)
+            direct_message = ' '.join(direct_message).strip()
+
+            if last_recipe != direct_message:
+                send_recipe()
+            else:
+                print('Search aborted: Recipe already found')
+
+        time.sleep(60)
+
+    except tweepy.RateLimitError as e:
+        logging.error("Twitter api rate limit reached".format(e))
+        time.sleep(60)
+        continue
